d850388b1d4b9df8da891bdfd63db1cf0ccef63d
SahidJafar <sahid11.sj@gmail.com>
2024-04-05 22:25:30 +0700

    feat: add sortCarByYearAscendingly
    using method sort and looping

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -7,7 +7,20 @@ function sortCarByYearAscendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
-
   // Rubah code ini dengan array hasil sorting secara ascending
+
+  // Menggunakan sort
+  // result.sort((firstItem, secondItem) => firstItem.year - secondItem.year);
+
+  for(let i = 0; i < result.length - 1; i++){
+    for(let j = 0; j < result.length - 1 - i; j++){
+      // Mengecek kondisi tahun sekarang lebih besar dari tahun setelahnya
+      if(result[j].year > result[j+1].year){
+        let temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
   return result;
 }
